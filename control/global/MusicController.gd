a2b3f55dbf3502ef786939950b72b22af6dbda21
Andy Bae <abae.yusung@gmail.com>
2021-01-01 19:14:00 -0500

    fixed menu dependencies

--- control/global/MusicController.gd
+++ control/global/MusicController.gd
@@ -1,20 +1,10 @@
 extends Node
 
-var menu_music = load("res://Music and Sounds/New Hope.ogg")
-var game_music = load("res://Music and Sounds/Common Fight.ogg")
-var victory_music = load("res://Music and Sounds/Chiptronical.ogg")
+#var menu_music = load("path to music")
 
 func play_menu_music():
-	if $Music.stream != menu_music:
-		$Music.stream = menu_music
-		$Music.play()
-	
-func play_game_music():
-	if $Music.stream != game_music:
-		$Music.stream = game_music
-		$Music.play()
+	pass
+#	if $Music.stream != menu_music:
+#		$Music.stream = menu_music
+#		$Music.play()
 
-func play_victory_music():
-	if $Music.stream != victory_music:
-		$Music.stream = victory_music
-		$Music.play()

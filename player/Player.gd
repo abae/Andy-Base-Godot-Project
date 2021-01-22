5a940a01b5d0c5c855af490d5a61fa34618c8f51
Andy Bae <abae.yusung@gmail.com>
2021-01-22 18:46:31 -0500

    converted to platformer base

--- player/Player.gd
+++ player/Player.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 export var MAX_SPEED = 80
 export var ACCEL = 500
 export var FRICT = 500
+export var GRAVITY = 800
+export var JUMP_SPEED = 600
+export var FALL_SPEED = 800
+
+export var SQUISHINESS = 0.08
 
 enum{
 	MOVE,
@@ -13,6 +18,7 @@ var vel = Vector2.ZERO
 var p_vel = vel
 var input = Vector2.ZERO
 var p_input = input
+var p_is_on_floor = true
 
 onready var level = get_tree().get_current_scene()
 onready var stats = GameState.playerStats
@@ -32,7 +38,7 @@ func _ready():
 	randomize()
 	stats.connect("no_health", self, "queue_free")
 
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
@@ -43,38 +49,50 @@ func _process(delta):
 func move_state(delta):
 	#get user input
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input = input.normalized()
+	if input.length() > 1.0:
+		input = input.normalized()
 	
-	#movement
+	#horizontal movement
 	if input != Vector2.ZERO:
-		vel = vel.move_toward(input * MAX_SPEED, ACCEL * delta)
+		vel.x = move_toward(vel.x, input.x * MAX_SPEED, ACCEL * delta)
 	else:
-		vel = vel.move_toward(Vector2.ZERO, FRICT * delta)
+		vel.x = move_toward(vel.x, 0, FRICT * delta)
 	
-	#sprite manipulation
-	squash_n_stretch()
-	if(input.x > 0 && p_input.x <= 0):
-		emit_dust($BottomLeft.global_position)
-	if(input.x < 0 && p_input.x >= 0):
-		emit_dust($BottomRight.global_position)
+	#vertical movement
+	vel.y = move_toward(vel.y, FALL_SPEED, GRAVITY * delta)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			vel.y -= JUMP_SPEED
 	
 	#calculate collisions
 	move()
+	
+	#sprite manipulation
+	squash_n_stretch()
+	if is_on_floor():
+		if(input.x > 0 && p_input.x <= 0):
+			emit_dust($BottomLeft.global_position)
+		if(input.x < 0 && p_input.x >= 0):
+			emit_dust($BottomRight.global_position)
+		if !p_is_on_floor:
+			emit_dust($BottomLeft.global_position)
+			emit_dust($BottomRight.global_position)
 
 func move():
-	vel = move_and_slide(vel)
+	vel = move_and_slide(vel, Vector2(0,-1))
 
 func squash_n_stretch():
-	var squash = 0.15*(abs(vel.x)/MAX_SPEED - abs(vel.y)/MAX_SPEED)
+	var squash = SQUISHINESS*(abs(vel.x)/MAX_SPEED - abs(vel.y)/MAX_SPEED)
 	var defaultScale = 0.31 #get rid of this once we actually use a sprite
 	sprite.scale = defaultScale * Vector2(1 + squash, 1 - squash)
+	sprite.offset.y = -2 * sprite.texture.get_size().y * (sprite.scale.y - defaultScale)
 
 func _on_DustTimer_timeout():
-	if(input.x > 0):
-		emit_dust($BottomLeft.global_position)
-	if(input.x < 0):
-		emit_dust($BottomRight.global_position)
+	if is_on_floor():
+		if(input.x > 0):
+			emit_dust($BottomLeft.global_position)
+		if(input.x < 0):
+			emit_dust($BottomRight.global_position)
 		
 func emit_dust(pos):
 	$DustTimer.start()
@@ -91,3 +109,4 @@ func _on_Hurtbox_area_entered(area):
 func update_p_values():
 	p_vel = vel
 	p_input = input
+	p_is_on_floor = is_on_floor()

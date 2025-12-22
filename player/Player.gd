55539bdbad918543299024b0da09c610556147d0
abae <abae.yusung@gmail.com>
2025-12-21 23:58:48 -0600

    added npc dialogue system prototype

--- player/Player.gd
+++ player/Player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 enum{
 	MOVE,
+	TALK
 }
 var state = MOVE
 var vel = Vector2.ZERO
@@ -23,11 +24,25 @@ func _ready():
 	randomize()
 	var _startingPosition = position
 	stats.connect("no_health", Callable(self, "queue_free"))
+	# Listen for Dialogic timeline end so we can return to MOVE state after talking
+	if typeof(Dialogic) != TYPE_NIL:
+		if Dialogic.has_subsystem('Text'):
+			var _text_sys = Dialogic.get_subsystem('Text')
+			if not _text_sys.is_connected('animation_textbox_hide', Callable(self, '_on_dialogue_hide')):
+				_text_sys.animation_textbox_hide.connect(Callable(self, '_on_dialogue_hide'))
+	else:
+		if Engine.has_singleton("Dialogic"):
+			var d = Engine.get_singleton("Dialogic")
+			if d and d.has_subsystem('Text'):
+				var _ts = d.get_subsystem('Text')
+				if not _ts.is_connected('animation_textbox_hide', Callable(self, '_on_dialogue_hide')):
+					_ts.animation_textbox_hide.connect(Callable(self, '_on_dialogue_hide'))
 
 func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
+			interact()
 	
 	#update to previous values
 	update_p_values()
@@ -50,6 +65,17 @@ func move_state(delta):
 	#calculate collisions
 	move()
 
+func interact():
+	#interact with objects
+	if Input.is_action_just_pressed("interact"):
+		# look for any nearby NPCs (they add themselves to the 'interactable_npcs' group)
+		for npc in get_tree().get_nodes_in_group("interactable_npcs"):
+			if npc.has_method("start_dialogue") and npc.player_in_range:
+				# trigger Dialogic timeline on the NPC
+				npc.start_dialogue()
+				state = TALK
+				return
+
 func move():
 	set_velocity(vel)
 	set_up_direction(Vector2(0,-1))
@@ -66,3 +92,5 @@ func _on_Hurtbox_area_entered(area):
 	$Hurtbox.start_invincibility(0.5)
 	$Hurtbox.create_hit_effect()
 
+func _on_dialogue_hide() -> void:
+	state = MOVE
